0336822815c0abcbbac7b1e431c029cd0ffdb74f
Rchi Lugtu <archeeeese.dev@gmail.com>
2025-09-16 10:59:13 +1200

    Add new DesignPage and a resuable table component with dummy data

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -5,6 +5,7 @@ import CompanyPage from "../Pages/CompanyPage/CompanyPage"
 import SearchPage from "../Pages/SearchPage/SearchPage"
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile"
 import IncomeStatements from "../Components/IncomeStatements/IncomeStatements"
+import DesignPage from "../Pages/DesignPage/DesignPage"
 
 //browser router - a factory that creates routes
 export const router = createBrowserRouter([
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
+      { path: "design-guide", element: <DesignPage /> },
       {
         path: "company/:ticker",
         element: <CompanyPage />,
