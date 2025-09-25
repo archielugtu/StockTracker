b30c36807a369566b660d07ca236b9cefe525daa
Rchi Lugtu <archeeeese.dev@gmail.com>
2025-09-26 00:37:12 +1200

    Add new dotnet web api

---
+++ api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // generates the swagger.json
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
