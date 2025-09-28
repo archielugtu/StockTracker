40ba212d8b57c7c7b080dbdee8dc51fdf2d1b431
Rchi Lugtu <archeeeese.dev@gmail.com>
2025-09-28 13:00:25 +1300

    Added Stock and Comment models

---
+++ api/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content{ get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        //Navigation property allows us to navigate into the stock model from the comment
+        public Stock? Stock { get; set; }
+    }
+}
