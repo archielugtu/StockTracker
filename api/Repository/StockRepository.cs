b231b428ca090543f9cf37df96773637fee40301
Rchi Lugtu <archeeeese.dev@gmail.com>
2025-10-16 00:03:54 +1300

    Add ability to sort stocks by symbol or company name

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -30,6 +30,14 @@ namespace api.Repository
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
 
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                    stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                else if (query.SortBy.Equals("CompanyName", StringComparison.OrdinalIgnoreCase))
+                    stocks = query.IsDescending ? stocks.OrderByDescending(s => s.CompanyName) : stocks.OrderBy(s => s.CompanyName);
+            }
+
             return await stocks.ToListAsync();
         }
 
