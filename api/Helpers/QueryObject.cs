55ebbd9b38cac39e65f0d39c86d317249d6ca9f9
Rchi Lugtu <archeeeese.dev@gmail.com>
2025-10-15 23:57:06 +1300

    add sorting properties to QueryObject

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -4,5 +4,7 @@
     {
         public string? Symbol { get; set; }
         public string? CompanyName { get; set; }
+        public string? SortBy { get; set; }
+        public bool IsDescending { get; set; } = false;
     }
 }
